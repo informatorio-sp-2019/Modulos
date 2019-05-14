8c5601c8a0664f374f00f7af05a7587ed8865027
matiasmf89 <mancuellomatias@gmail.com>
2019-05-14 18:40:58 -0300

    valida_usu: método valida longitud mínima

---
+++ validacion/valida_usu.py
@@ -0,0 +1,14 @@
+"""
+•	El nombre de usuario debe contener un mínimo de 6 caracteres y un máximo de 12.
+•	El nombre de usuario debe ser alfanumérico.
+
+"""
+
+def valida_long_min(usuario):
+	if len(usuario) < 6:
+		lRetorno = False
+	else:
+		lRetorno = True	
+
+	return lRetorno
+
