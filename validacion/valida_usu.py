cde286a3438c5bc3227aa40177d0ede6a04ac8b8
Edgardo <edgardo951.ef@gmail.com>
2019-05-14 19:25:27 -0300

    metodo main

--- validacion/valida_usu.py
+++ validacion/valida_usu.py
@@ -19,3 +19,36 @@ def valida_long_max(usuario):
 		lRetorno = True
 
 	return lRetorno
+
+def __main():
+	""" ésta función está definida como privada, sólo puede accederse desde
+	    éste módulo, y no desde otro, cuando es importado. 
+	"""
+	while True:
+		from os import system
+		system('cls')
+		usuario = input('Usuario: ')
+		if not valida_long_min(usuario):
+			print('\n Debe ingresar un mínimo de 6 caracteres!')
+			input()			
+			continue
+
+		if not valida_long_max(usuario):
+			print('\n Debe ingresar un máximo de 12 caracteres!')
+			input()			
+			continue
+
+		if not valida_alfanum(usuario):
+			print('\n El nombre de usuario sólo puede estar compuesto por letras y números!')
+			input()			
+			continue
+
+		
+		print('\n ¡Usuario validado!')
+		break
+
+if __name__ == '__main__':	
+	""" por aquí entrará cuando se ejecute éste módulo como principal, 
+	    y no cuando importemos desde ótro módulo. 
+	"""
+	__main()
