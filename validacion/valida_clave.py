3a55ff46b234ba72fab59adc9bc46379248269c3
Ivana <ivanasosacordero@gmail.com>
2019-05-15 19:17:45 -0300

    Se agregó función para identificar caracter no alfanumérico

--- validacion/valida_clave.py
+++ validacion/valida_clave.py
@@ -6,3 +6,15 @@ MÓDULO VALIDA CLAVE
 •	Contraseña válida, retorna True.
 •	Contraseña no válida, retorna el mensaje "La contraseña elegida no es segura".
 """
+
+
+#Función para validar caracter no alfanumérico
+
+def valida_caracter_no_alfanum(clave):
+    lRetorno = False
+    for c in clave:
+        if not c.isalnum():
+            lRetorno = True
+            break
+
+    return lRetorno   
