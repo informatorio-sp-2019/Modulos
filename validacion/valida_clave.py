7fe2e80ee7240ebcad948adb71d997acda5266ce
matiasmf89 <mancuellomatias@gmail.com>
2019-05-15 19:03:49 -0300

    inicio valida clave.

---
+++ validacion/valida_clave.py
@@ -0,0 +1,8 @@
+"""
+MÓDULO VALIDA CLAVE
+•	La contraseña debe contener un mínimo de 8 caracteres.
+•	Una contraseña debe contener letras minúsculas, mayúsculas, números y al menos 1 carácter no alfanumérico.
+•	La contraseña no puede contener espacios en blanco.
+•	Contraseña válida, retorna True.
+•	Contraseña no válida, retorna el mensaje "La contraseña elegida no es segura".
+"""
