2b74e550221d2420bf82f49bda005b2b90c5b15c
Hector <hector_ing2018@hotmail.com>
2019-05-15 19:21:12 +0100

    Se agrega la funcion que valida la longitud de la clave

--- validacion/valida_clave.py
+++ validacion/valida_clave.py
@@ -6,3 +6,15 @@ MÓDULO VALIDA CLAVE
 •	Contraseña válida, retorna True.
 •	Contraseña no válida, retorna el mensaje "La contraseña elegida no es segura".
 """
+def valida_long_min(clave):
+	"""
+	+ recibe un str
+	+ evalua longitud mínima de 8
+	+ retorna un valor booleano
+	"""
+	if len(clave) < 8:
+		lRetorno = False
+	else:
+		lRetorno = True
+
+	return lRetorno
